240e2b9c566d78dbefd95bc01438c4f6921a5f84
nkmt <nfmail9@gmail.com>
2019-06-29 14:17:10 +0800

    implement redux component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { crateStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers';
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
