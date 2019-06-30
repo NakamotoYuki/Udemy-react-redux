a908584fb4790e56c264898ae39043ae51b3b598
nkmt <nfmail9@gmail.com>
2019-06-30 09:56:36 +0800

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { crateStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import './index.css';
